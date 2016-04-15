dcedc5bdb1a84c0af6c660a57040504a2071ff92
Gaudy <gbm2494@hotmail.com>
2016-04-15 12:06:40 -0600

    cambio entity

--- GuiasOET/GuiasOET/Models/Modelo.Designer.cs
+++ GuiasOET/GuiasOET/Models/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Gaudy-PC\UCR\2016 I Ciclo\Ingeniería de Software II\Proyecto\SistemaGuiasOET\GuiasOET\GuiasOET\Models\Modelo.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\gblanco\Desktop\SistemaGuiasOET\GuiasOET\GuiasOET\Models\Modelo.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
