74a9a3521f0173f9c77e2baeba8bbe7f36d5a7ab
LuisLeandro09@gmail.com <lule1994>
2016-05-13 19:07:05 -0600

    ConsultarAsignacion

---
+++ GuiasOET/GuiasOET/Controllers/AsignacionReservacionesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuiasOET.Models;
+using System.Diagnostics;
+using System.Data.Entity.Infrastructure;
+using System.Net;
+using System.Web.UI.HtmlControls;
+using System.Data.Entity;
+using PagedList;
+using MvcFlashMessages;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace GuiasOET.Controllers
+{
+    public class AsignacionReservacionesController : Controller
+    {
+
+
+     
+        public ActionResult ConsultarAsignacion()
+        {
+            return View();
+        }
+
+
+
+    }
+}
