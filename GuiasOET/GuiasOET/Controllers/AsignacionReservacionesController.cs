d8fe87c22e24e78b4c16a5fc6b1d074dcfcc673b
Gaudy <gbm2494@hotmail.com>
2016-05-17 15:49:48 -0600

    INICIO DE ASIGNAR RESERVACIONES

--- GuiasOET/GuiasOET/Controllers/AsignacionReservacionesController.cs
+++ GuiasOET/GuiasOET/Controllers/AsignacionReservacionesController.cs
@@ -23,6 +23,76 @@ namespace GuiasOET.Controllers
         private Entities1 baseDatos = new Entities1();
 
 
+        public ActionResult AsignarReservacion(string sortOrder, string currentFilter, string fechaDesde, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.ReservacionSortParm = String.IsNullOrEmpty(sortOrder) ? "Reservacion" : "";
+            ViewBag.NombreSortParm = String.IsNullOrEmpty(sortOrder) ? "Nombre" : "";
+            ViewBag.EstacionSortParm = String.IsNullOrEmpty(sortOrder) ? "Estacion" : "";
+            ViewBag.PersonasSortParm = String.IsNullOrEmpty(sortOrder) ? "Personas" : "";
+            ViewBag.FechaSortParm = String.IsNullOrEmpty(sortOrder) ? "Fecha" : "";
+            ViewBag.HoraSortParm = String.IsNullOrEmpty(sortOrder) ? "Hora" : "";
+            ViewBag.TurnoSortParm = String.IsNullOrEmpty(sortOrder) ? "Turno" : "";
+            ViewBag.GuiasAsignadosSortParm = String.IsNullOrEmpty(sortOrder) ? "Guías Asignados" : "";
+
+            if (fechaDesde != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                fechaDesde = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = fechaDesde;
+
+            var empleados = from e in baseDatos.GUIAS_EMPLEADO select e;
+
+            if (!String.IsNullOrEmpty(fechaDesde))
+            {
+                empleados = empleados.Where(e => e.APELLIDO1.Contains(fechaDesde)
+                                       || e.NOMBREEMPLEADO.Contains(fechaDesde) || e.APELLIDO2.Contains(fechaDesde)
+                                       || e.NOMBREESTACION.Contains(fechaDesde) || e.TIPOEMPLEADO.Contains(fechaDesde)
+                                       || e.USUARIO.Contains(fechaDesde) || e.EMAIL.Contains(fechaDesde));
+            }
+
+
+            switch (sortOrder)
+            {
+                case "Reservacion":
+                    empleados = empleados.OrderBy(e => e.NOMBREEMPLEADO);
+                    break;
+                case "Nombre":
+                    empleados = empleados.OrderBy(e => e.APELLIDO1);
+                    break;
+                case "Estacion":
+                    empleados = empleados.OrderBy(e => e.APELLIDO2);
+                    break;
+                case "Personas":
+                    empleados = empleados.OrderBy(e => e.NOMBREESTACION);
+                    break;
+                case "Fecha":
+                    empleados = empleados.OrderBy(e => e.TIPOEMPLEADO);
+                    break;
+                case "Hora":
+                    empleados = empleados.OrderBy(e => e.APELLIDO2);
+                    break;
+                case "Turno":
+                    empleados = empleados.OrderBy(e => e.NOMBREESTACION);
+                    break;
+                case "Guías Asignados":
+                    empleados = empleados.OrderBy(e => e.TIPOEMPLEADO);
+                    break;
+                default:
+                    empleados = empleados.OrderBy(e => e.NOMBREESTACION);
+                    break;
+            }
+
+            int pageSize = 8;
+            int pageNumber = (page ?? 1);
+            return View(empleados.ToPagedList(pageNumber, pageSize));
+        }
+
         public ActionResult ConsultarAsignacion()
         {
             return View();
