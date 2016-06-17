6e8d233f99ecf0ac3e2b706a9d78c3d5bcfe0c3a
leandro09 <luisleandro09@gmail.com>
2016-06-17 12:17:32 -0600

    Avances en PersonasAtendidas

--- GuiasOET/GuiasOET/Controllers/ReportesController.cs
+++ GuiasOET/GuiasOET/Controllers/ReportesController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using GuiasOET.Models;
+using System.Collections;
 
 namespace GuiasOET.Controllers
 {
@@ -30,21 +31,38 @@ namespace GuiasOET.Controllers
             //Todas las reservaciones del sistema
             var reservacion = from r in baseDatos.GUIAS_RESERVACION select r;
 
-
-
-
-
-
-
-
-
             return View();
         }
 
-        public ActionResult generarReporte()
+        public ActionResult PersonasAtendidas(string fechaDesde, string fechaHasta)
         {
+            ReportesModelo reportes;
+            DateTime fechaD = Convert.ToDateTime(fechaHasta);
+            DateTime fechaH = Convert.ToDateTime(fechaHasta);
+            List<GUIAS_RESERVACION> reservaciones = baseDatos.GUIAS_RESERVACION.Where(e => e.FECHA < fechaH && e.FECHA > fechaD).ToList();
+
+            reservaciones = reservaciones.OrderBy(r=> r.FECHA).ToList();
+
+            var asignaciones = from r in baseDatos.GUIAS_ASIGNACION select r;
+
+
 
             return View("ReportesReservaciones");
+        }
+
+        public ActionResult Reservaciones()
+        {
+
+
+
+
+            return View("ReportesReservaciones");
+        }
+
+
+        public void calcularSubTotales()
+        {
+
         }
     }
 }
--- GuiasOET/GuiasOET/Models/ReportesModelo.cs
+++ GuiasOET/GuiasOET/Models/ReportesModelo.cs
@@ -9,14 +9,17 @@ namespace GuiasOET.Models
     public class ReportesModelo
     {
 
-        public IPagedList<GUIAS_RESERVACION> reservaciones;
-        public IPagedList<IEnumerable<GUIAS_EMPLEADO>> empleados;
-        public IPagedList<GUIAS_ASIGNACION> reservacionesAsignadas;
+        public List<GUIAS_RESERVACION> reservaciones = new List<GUIAS_RESERVACION>();
+        public List<IEnumerable<GUIAS_EMPLEADO>> empleados = new List<IEnumerable<GUIAS_EMPLEADO>>();
+        public List<GUIAS_ASIGNACION> reservacionesAsignadas = new List<GUIAS_ASIGNACION>();
         public IPagedList<GUIAS_RESERVACION> totalReservaciones { get; set; }
+        public List<DateTime> fechasReservaciones = new List<DateTime>();
+        public List<List<int>> subTotales = new List<List<int>>();
+
 
         public ReportesModelo()
         {
-
+           
         }
     }
 }
